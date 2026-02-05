3ef80c43f7bd1dbe48e29048d38a576b395973f7
Sankeerth Nara <sankeerthnara@gmail.com>
2026-02-05 06:31:17 +0000

    Sync LeetCode submission - Insert into a Binary Search Tree (c)

---
+++ problems/insert_into_a_binary_search_tree/solution.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+ struct TreeNode* create(int val)
+ {
+    struct TreeNode* new=(struct TreeNode*)malloc(sizeof(struct TreeNode));
+    new->val=val;
+    new->left=NULL;
+    new->right=NULL;
+    return new;
+ }
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+    if(root==NULL)
+    {
+        root=create(val);
+    }
+    else
+    {
+        if(root->val<val) root->right=insertIntoBST(root->right,val);
+        else root->left=insertIntoBST(root->left,val);
+    }
+    return root;
+}
