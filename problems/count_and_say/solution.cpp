43dfe5d2d56ef41da54a1f050f6f9199b3c6fe1b
Sankeerth Nara <sankeerthnara@gmail.com>
2026-06-13 17:39:44 +0000

    Sync LeetCode submission - Count and Say (cpp)

---
+++ problems/count_and_say/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string ans = "1";
+
+        for (int i = 2; i <= n; i++) {
+            char prev = ans[0];
+            string temp = "";
+            int count = 1;
+
+            for (int j = 1; j < ans.size(); j++) {
+                if (ans[j] == prev) {
+                    count++;
+                } else {
+                    temp += to_string(count);
+                    temp += prev;
+
+                    prev = ans[j];
+                    count = 1;
+                }
+            }
+
+            temp += to_string(count);
+            temp += prev;
+
+            ans = temp;
+        }
+
+        return ans;
+    }
+};
