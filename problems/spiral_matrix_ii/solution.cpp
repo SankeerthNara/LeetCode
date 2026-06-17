dfc2a2c6e9017a96fa302b3bf0a7c61f4bb94061
Sankeerth Nara <sankeerthnara@gmail.com>
2026-06-17 15:05:06 +0000

    Sync LeetCode submission - Spiral Matrix II (cpp)

---
+++ problems/spiral_matrix_ii/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> ans(n,vector<int>(n));
+        int k = 1;
+        int top = 0;
+        int left = 0;
+        int bottom = n-1;
+        int right = n-1;
+        while(left<=right && top <= bottom)
+        {
+            for(int i = left ; i<=right ; i++)
+            {
+                ans[top][i] = k++;
+            }
+            top++;
+            for(int i = top ; i<= bottom ; i++)
+            {
+                ans[i][right] = k++;
+            }
+            right--;
+            for(int i = right ; i>=left ; i -- )
+            {
+                ans[bottom][i] = k++;
+            }
+            bottom--;
+            for(int i = bottom ; i>=top ; i--)
+            {
+                ans[i][left] = k++;
+            }
+            left ++;
+        }
+        return ans;
+    }
+};
