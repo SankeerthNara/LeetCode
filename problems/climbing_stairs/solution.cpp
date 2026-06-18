eec100b15e96d4deb5de145379003c9f6ff40c26
Sankeerth Nara <sankeerthnara@gmail.com>
2026-06-18 12:59:54 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

---
+++ problems/climbing_stairs/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 0) return 0;
+        int one = 1;
+        int two = 1;
+        int i = 1;
+        while(i<n)
+        {
+            int t = two;
+            two = one+two;
+            one = t;
+            i++;
+        }
+        return two;
+    }
+};
