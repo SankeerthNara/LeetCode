c5628d8e494c0e8a462afa87b37d75916d9a3a40
Sankeerth Nara <sankeerthnara@gmail.com>
2026-06-21 14:17:05 +0000

    Sync LeetCode submission - Subsets II (cpp)

---
+++ problems/subsets_ii/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> curr;
+
+    void solve(vector<int>& nums, int idx) {
+        ans.push_back(curr);
+
+        for (int i = idx; i < nums.size(); i++) {
+            if (i > idx && nums[i] == nums[i - 1])
+                continue;   // skip duplicate subsets
+
+            curr.push_back(nums[i]);
+            solve(nums, i + 1);
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        solve(nums, 0);
+        return ans;
+    }
+};
