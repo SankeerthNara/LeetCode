821b21af7f2d4794722d7d1995fca7490b53dbf7
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:35:16 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/9/2026, 10:35:12 PM
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> curr;
+    void vatta (vector<int>&nums,int n,int idx)
+    {
+        if(idx >= n)
+        {
+            ans.push_back(curr);
+            return;
+        }
+        vatta(nums,n,idx+1);
+        curr.push_back(nums[idx]);
+        vatta(nums,n,idx+1);
+        curr.pop_back();
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vatta(nums,n,0);
+        // sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
