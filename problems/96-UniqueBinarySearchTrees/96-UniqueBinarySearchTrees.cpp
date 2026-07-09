d90dbbedcdfafc628e8203f48de8bd9a9a6f0819
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:24:03 +0530

    Create 96-UniqueBinarySearchTrees

---
+++ problems/96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 10:23:59 PM
+class Solution {
+public:
+    int numTrees(int n) {
+
+        vector<int> dp(n + 1, 0);
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int nodes = 2; nodes <= n; nodes++)
+        {
+            for(int root = 1; root <= nodes; root++)
+            {
+                dp[nodes] += dp[root - 1] *
+                             dp[nodes - root];
+            }
+        }
+
+        return dp[n];
+    }
+};
