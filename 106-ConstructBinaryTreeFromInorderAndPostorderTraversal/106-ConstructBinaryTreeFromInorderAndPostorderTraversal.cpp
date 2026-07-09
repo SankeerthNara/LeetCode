029011b8d2441b03f030074a4b7ca112d45d43a3
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:34:25 +0530

    Create 106-ConstructBinaryTreeFromInorderAndPostorderTraversal

---
+++ 106-ConstructBinaryTreeFromInorderAndPostorderTraversal/106-ConstructBinaryTreeFromInorderAndPostorderTraversal.cpp
@@ -0,0 +1,36 @@
+// Last updated: 7/9/2026, 10:34:21 PM
+class Solution {
+public:
+    unordered_map<int, int> pos;
+    int postIndex;
+
+    TreeNode* build(vector<int>& inorder,
+                    vector<int>& postorder,
+                    int left,
+                    int right) {
+
+        if (left > right)
+            return nullptr;
+
+        int rootVal = postorder[postIndex--];
+        TreeNode* root = new TreeNode(rootVal);
+
+        int mid = pos[rootVal];
+
+        root->right = build(inorder, postorder, mid + 1, right);
+        root->left  = build(inorder, postorder, left, mid - 1);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& inorder,
+                        vector<int>& postorder) {
+
+        for (int i = 0; i < inorder.size(); i++)
+            pos[inorder[i]] = i;
+
+        postIndex = postorder.size() - 1;
+
+        return build(inorder, postorder, 0, inorder.size() - 1);
+    }
+};
