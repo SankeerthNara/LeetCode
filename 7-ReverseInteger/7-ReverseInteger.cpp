b3e479f5ed9fd2acb5303fe4bd8b93fb8d359832
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:37:09 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 10:37:05 PM
+class Solution {
+public:
+    int reverse(int x) {
+        long long q = 0;
+        int sign = 0;
+        long long p =x;
+        if(x< 0)
+        {
+            p=-p;
+            sign = 1;
+        }
+        while(p)
+        {
+            q = q*10 + (long long)p%10;
+            p = p/10;
+        }
+        if(sign == 1)
+        {
+            q = -q;
+        }
+        int ans = q;
+        if(ans == q)
+        {
+            return ans;
+        }
+        else return 0;
+    }
+};
