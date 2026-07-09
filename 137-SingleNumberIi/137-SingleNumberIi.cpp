3053f2ca531064e92175f0f32baabf4dc373a0bf
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:33:37 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 10:33:33 PM
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i = 0;
+        while(i < nums.size()-1)
+        {
+            if(nums[i]!=nums[i+1]) return nums[i];
+            else
+            {
+                i+=3;
+            }
+        }
+        return nums[i];
+    }
+};
