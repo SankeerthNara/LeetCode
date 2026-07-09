81ee335be435aab0912b7aff0cce254566ad19f1
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:35:33 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 10:35:29 PM
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        reverse(digits.begin(),digits.end());
+        int carry = 1;
+        int n = digits.size();
+        int i = 0;
+        while(i<n)
+        {
+            digits[i] = digits[i]+carry;
+            carry = digits[i]/10;
+            digits[i] = digits[i]%10;
+            i++;
+        } 
+        if(carry) digits.push_back(carry);
+        reverse(digits.begin(),digits.end());
+        return digits;
+    }
+};
