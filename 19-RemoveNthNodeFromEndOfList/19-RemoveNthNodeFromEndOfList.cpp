d69aea48f6488275540f3f55249d02bead712ecd
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:36:52 +0530

    Create 19-RemoveNthNodeFromEndOfList

---
+++ 19-RemoveNthNodeFromEndOfList/19-RemoveNthNodeFromEndOfList.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 10:36:48 PM
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode* fast = &dummy;
+        ListNode* slow = &dummy;
+
+        // Move fast n+1 steps ahead
+        for(int i = 0; i <= n; i++) {
+            fast = fast->next;
+        }
+
+        // Move both pointers
+        while(fast) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        // Delete nth node from end
+        slow->next = slow->next->next;
+
+        return dummy.next;
+    }
+};
