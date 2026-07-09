10c6872b92233142073027b309f3ac50bdf5114f
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:33:40 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 10:33:36 PM
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int i = 0;
+        sort(nums.begin(),nums.end());
+        while( i < nums.size()-1)
+        {
+            if(nums[i]!=nums[i+1]) return nums[i];
+            else
+            {
+                while(i < nums.size() && nums[i] == nums[i+1]) i++;
+                i++;
+            }
+        }
+        return nums[i];
+    }
+};
