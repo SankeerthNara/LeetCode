6180cd4b0b6bb49399ea71d2a7eb851b3b4574bd
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:23:26 +0530

    Create 114-FlattenBinaryTreeToLinkedList

---
+++ problems/114-FlattenBinaryTreeToLinkedList/114-FlattenBinaryTreeToLinkedList.cpp
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 10:23:22 PM
+class Solution {
+public:
+    vector<TreeNode*> nodes;
+
+    void preorder(TreeNode* root) {
+        if (!root) return;
+
+        nodes.push_back(root);
+        preorder(root->left);
+        preorder(root->right);
+    }
+
+    void flatten(TreeNode* root) {
+        if(!root) return;
+        preorder(root);
+
+        for (int i = 0; i < nodes.size() - 1; i++) {
+            nodes[i]->left = nullptr;
+            nodes[i]->right = nodes[i + 1];
+        }
+
+        if (!nodes.empty()) {
+            nodes.back()->left = nullptr;
+            nodes.back()->right = nullptr;
+        }
+    }
+};
