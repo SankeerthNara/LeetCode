69b1fcb16ccd4c76e3bf56b284fa374ae8d2cf6e
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:22:47 +0530

    Create 141-LinkedListCycle

---
+++ problems/141-LinkedListCycle/141-LinkedListCycle.c
@@ -0,0 +1,39 @@
+// Last updated: 7/9/2026, 10:22:43 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *fast=head;
+    struct ListNode *slow=head;
+    if(head==NULL)
+    {
+        return 0;
+    }
+    if(head->next==NULL)
+    {
+        return 0;
+    }
+    if(head->next->next==NULL)
+    {
+        return 0;
+    }
+    slow=slow->next;
+    fast=fast->next->next;
+    while(fast!=NULL&&fast->next!=NULL&&fast->next->next!=NULL)
+    {
+       if(slow==fast)
+       {
+        return 1;
+       }
+       else
+       {
+        slow=slow->next;
+        fast=fast->next->next;
+       }
+    }   
+    return 0; 
+}
