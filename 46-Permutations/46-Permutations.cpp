c48c057b1cc19ecd8e88498b6ef00a202d2aeb9f
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:36:02 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 10:35:58 PM
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void solve(vector<int>& nums, int idx) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[idx], nums[i]);
+            solve(nums, idx + 1);
+            swap(nums[idx], nums[i]); // backtrack
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        solve(nums, 0);
+        return ans;
+    }
+};
