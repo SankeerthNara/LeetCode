b250e64a5b115472d2ab30f15ff2a683ad5ce33e
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:35:47 +0530

    Create 57-InsertInterval

---
+++ 57-InsertInterval/57-InsertInterval.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 10:35:43 PM
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+
+        for (auto &interval : intervals) {
+            if (ans.empty() || ans.back()[1] < interval[0]) {
+                ans.push_back(interval);
+            } else {
+                ans.back()[1] = max(ans.back()[1], interval[1]);
+            }
+        }
+
+        return ans;
+    }
+};
