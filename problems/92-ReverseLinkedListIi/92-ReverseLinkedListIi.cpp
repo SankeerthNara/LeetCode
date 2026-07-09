09b8aca3ac44a4aee346a21348210ae583a2671a
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:24:09 +0530

    Create 92-ReverseLinkedListIi

---
+++ problems/92-ReverseLinkedListIi/92-ReverseLinkedListIi.cpp
@@ -0,0 +1,30 @@
+// Last updated: 7/9/2026, 10:24:05 PM
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+
+        if(!head || left == right)
+            return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode* prev = &dummy;
+
+        for(int i = 1; i < left; i++)
+            prev = prev->next;
+
+        ListNode* curr = prev->next;
+
+        for(int i = 0; i < right-left; i++)
+        {
+            ListNode* temp = curr->next;
+
+            curr->next = temp->next;
+            temp->next = prev->next;
+            prev->next = temp;
+        }
+
+        return dummy.next;
+    }
+};
