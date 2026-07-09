bfe24a014e45a787b446619479a19ac10d133cc6
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:36:44 +0530

    Create 24-SwapNodesInPairs

---
+++ 24-SwapNodesInPairs/24-SwapNodesInPairs.cpp
@@ -0,0 +1,36 @@
+// Last updated: 7/9/2026, 10:36:40 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp!=NULL && temp->next!=NULL)
+        {
+            ListNode* t = temp->next->next;
+            ListNode* t1 = temp->next;
+            temp->next = t;
+            t1->next = temp;
+            if(prev == NULL)
+            {
+                head = t1;
+            }
+            else
+            {
+                prev->next = t1;
+            }
+            prev = temp;
+            temp = temp->next;
+        }
+        return head;
+    }
+};
