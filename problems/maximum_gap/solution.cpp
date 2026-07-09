d364076840c62e4c8c63071f984d93d935ad062f
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 15:26:42 +0000

    Sync LeetCode submission - Maximum Gap (cpp)

---
+++ problems/maximum_gap/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if(nums.size() < 2) return 0;
+        sort(nums.begin(),nums.end());
+        int ans = nums[1] - nums[0];
+        for(int i =2 ; i < nums.size() ; i++)
+        {
+            ans = max(nums[i]-nums[i-1],ans);
+        }
+        return ans;   
+    }
+};
