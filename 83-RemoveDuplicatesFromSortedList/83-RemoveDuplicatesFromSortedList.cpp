077c31f064b639e700dbbbdae25f837a8ac05f9d
Sankeerth Nara <sankeerthnara@gmail.com>
2026-07-09 22:35:08 +0530

    Create 83-RemoveDuplicatesFromSortedList

---
+++ 83-RemoveDuplicatesFromSortedList/83-RemoveDuplicatesFromSortedList.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 10:35:04 PM
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* curr = head;
+
+        while (curr && curr->next) {
+            if (curr->val == curr->next->val)
+                curr->next = curr->next->next;
+            else
+                curr = curr->next;
+        }
+
+        return head;
+    }
+};
