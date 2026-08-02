b3cb2b876ce2202dc9190aa69f5440ec7f3b7867
Sankeerth Nara <sankeerthnara@gmail.com>
2026-08-02 17:27:22 +0530

    File updated at 8/2/2026, 5:27:20 PM

--- 283-MoveZeroes/283-MoveZeroes.cpp
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,29 +1,16 @@
-// Last updated: 8/2/2026, 5:21:42 PM
-1class PeekingIterator : public Iterator {
-2private:
-3    int nextVal;
-4    bool has_next;
-5
-6public:
-7    PeekingIterator(const vector<int>& nums) : Iterator(nums) {
-8        has_next = Iterator::hasNext();
-9        if (has_next)
-10            nextVal = Iterator::next();
-11    }
-12
-13    int peek() {
-14        return nextVal;
-15    }
-16
-17    int next() {
-18        int val = nextVal;
-19        has_next = Iterator::hasNext();
-20        if (has_next)
-21            nextVal = Iterator::next();
-22        return val;
-23    }
-24
-25    bool hasNext() const {
-26        return has_next;
-27    }
-28};
+// Last updated: 8/2/2026, 5:27:20 PM
+1class Solution {
+2public:
+3    int findDuplicate(vector<int>& nums) {
+4        unordered_set <int> st;
+5        for(int i : nums)
+6        {
+7            if(!st.count(i))
+8            {
+9                st.insert(i);
+10            }
+11            else return i;
+12        }
+13        return -1;
+14    }
+15};
