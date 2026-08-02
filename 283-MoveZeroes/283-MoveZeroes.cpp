b33e34a8dadcd798fc1c4a2222fc1691e709cd17
Sankeerth Nara <sankeerthnara@gmail.com>
2026-08-02 17:21:43 +0530

    File updated at 8/2/2026, 5:21:42 PM

--- 283-MoveZeroes/283-MoveZeroes.cpp
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,16 +1,29 @@
-// Last updated: 8/2/2026, 5:10:28 PM
-1class Solution {
-2public:
-3    void moveZeroes(vector<int>& nums) {
-4       int k = 0;
-5       for(int i : nums)
-6       {
-7            if(i!=0) nums[k++] = i;
-8       }
-9       for(int i = k ; i < nums.size() ; i++)
-10       {
-11        nums[i] = 0;
-12       }
-13       return;
-14    }
-15};
+// Last updated: 8/2/2026, 5:21:42 PM
+1class PeekingIterator : public Iterator {
+2private:
+3    int nextVal;
+4    bool has_next;
+5
+6public:
+7    PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+8        has_next = Iterator::hasNext();
+9        if (has_next)
+10            nextVal = Iterator::next();
+11    }
+12
+13    int peek() {
+14        return nextVal;
+15    }
+16
+17    int next() {
+18        int val = nextVal;
+19        has_next = Iterator::hasNext();
+20        if (has_next)
+21            nextVal = Iterator::next();
+22        return val;
+23    }
+24
+25    bool hasNext() const {
+26        return has_next;
+27    }
+28};
