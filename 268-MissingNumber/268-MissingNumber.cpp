8065dfba1a89217b7ae434441abf521ce6b988e5
Sankeerth Nara <sankeerthnara@gmail.com>
2026-08-02 12:29:25 +0530

    File updated at 8/2/2026, 12:29:24 PM

--- 268-MissingNumber/268-MissingNumber.cpp
+++ 268-MissingNumber/268-MissingNumber.cpp
@@ -1,10 +1,16 @@
-// Last updated: 8/2/2026, 12:13:06 PM
+// Last updated: 8/2/2026, 12:29:24 PM
 1class Solution {
 2public:
-3    int missingNumber(vector<int>& nums) {
-4        int n = nums.size();
-5        int sum = accumulate(nums.begin(),nums.end(),0);
-6        int ac = (n*(n+1))/2;
-7        return ac-sum;
-8    }
-9};
+3    int hIndex(vector<int>& citations) {
+4        sort(citations.begin(), citations.end(), greater<int>());
+5
+6        int n = citations.size();
+7
+8        for (int i = 0; i < n; i++) {
+9            if (citations[i] < i + 1)
+10                return i;
+11        }
+12
+13        return n;
+14    }
+15};
