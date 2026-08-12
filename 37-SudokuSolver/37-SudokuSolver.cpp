95b1d1a96b315db151f1b7701867ed48be374d65
Sankeerth Nara <sankeerthnara@gmail.com>
2026-08-12 22:46:49 +0530

    File updated at 8/12/2026, 10:46:48 PM

--- 37-SudokuSolver/37-SudokuSolver.cpp
+++ 37-SudokuSolver/37-SudokuSolver.cpp
@@ -1,27 +1,56 @@
-// Last updated: 8/12/2026, 10:37:28 PM
+// Last updated: 8/12/2026, 10:46:48 PM
 1class Solution {
 2public:
-3    int firstMissingPositive(vector<int>& nums) {
-4        int n = nums.size();
-5        vector<bool> seen(n + 1, false);  // Array for lookup
-6
-7        // Mark the elements from nums in the lookup array
-8        for (int num : nums) {
-9            if (num > 0 && num <= n) {
-10                seen[num] = true;
-11            }
-12        }
-13
-14        // Iterate through integers 1 to n
-15        // return smallest missing positive integer
-16        for (int i = 1; i <= n; i++) {
-17            if (!seen[i]) {
-18                return i;
-19            }
-20        }
-21
-22        // If seen contains all elements 1 to n
-23        // the smallest missing positive number is n + 1
-24        return n + 1;
-25    }
-26};
+3    bool isValid(vector<vector<char>>& board, int r, int c, char ch) {
+4        for (int i = 0; i < 9; i++) {
+5            // Row
+6            if (board[r][i] == ch)
+7                return false;
+8
+9            // Column
+10            if (board[i][c] == ch)
+11                return false;
+12
+13            // 3x3 box
+14            int nr = 3 * (r / 3) + i / 3;
+15            int nc = 3 * (c / 3) + i % 3;
+16
+17            if (board[nr][nc] == ch)
+18                return false;
+19        }
+20
+21        return true;
+22    }
+23
+24    bool solve(vector<vector<char>>& board) {
+25        for (int r = 0; r < 9; r++) {
+26            for (int c = 0; c < 9; c++) {
+27
+28                if (board[r][c] == '.') {
+29
+30                    for (char ch = '1'; ch <= '9'; ch++) {
+31
+32                        if (isValid(board, r, c, ch)) {
+33                            board[r][c] = ch;
+34
+35                            if (solve(board))
+36                                return true;
+37
+38                            // Backtrack
+39                            board[r][c] = '.';
+40                        }
+41                    }
+42
+43                    // No digit works
+44                    return false;
+45                }
+46            }
+47        }
+48
+49        return true;
+50    }
+51
+52    void solveSudoku(vector<vector<char>>& board) {
+53        solve(board);
+54    }
+55};
